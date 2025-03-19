30828ce426fbd366016dcba412e6d20daf9b1985
dsgnr <email@danielhand.io>
2025-03-19 20:25:08 +0000

    Add frontend packaging

---
+++ frontend/web/webpack.config.js
@@ -0,0 +1,50 @@
+const { resolve } = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: ["./src/js/index.js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            minify: {
+                collapseWhitespace: true,
+            },
+        }),
+    ],
+    devServer: {
+        port: 8080,
+        proxy: [
+            {
+                context: ["/api"],
+                target: process.env.API_URL,
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new UglifyJsPlugin({
+                include: /\.js$/,
+            }),
+            new CssMinimizerPlugin(),
+        ],
+    },
+    output: {
+        path: resolve(__dirname, "dist"),
+        filename: "js/[name].min.js",
+    },
+};
